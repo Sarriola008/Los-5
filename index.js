fb3f9310dc47c31a355cdeef4f919a8a4989a8bd
Sarriola008 <sarrmig093@g.educaand.es>
2025-01-22 20:34:55 +0100

    Cambios 4
    x

---
+++ index.js
@@ -0,0 +1,43 @@
+let Paco
+Paco=prompt("Escribe tu nombre");
+if(Paco==("")){
+    document.write("Tu nombre no es Paco")
+
+}else {
+    document.write("Tu nombre es el correcto")
+};
+let Jesus
+Jesus=prompt("Escribe tu nombre");
+if(Jesuss==("")){
+    document.write("Tu nombre no es Jesus")
+
+}else {
+    document.write("Tu nombre es el correcto")
+};
+let Pablo
+Pablo=prompt("Escribe tu nombre");
+if(Pablo==("")){
+    document.write("Tu nombre no es Pablo")
+
+}else {
+    document.write("Tu nombre es el correcto")
+};
+let Santiago
+Santiago=prompt("Escribe tu nombre");
+if(Santiago==("")){
+    document.write("Tu nombre no es Santiago")
+
+}else {
+    document.write("Tu nombre es el correcto")
+};
+
+
+
+
+
+
+
+
+
+
+
