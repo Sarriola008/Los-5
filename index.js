429d3820dee6a4feb49cd375156cd58284bf6b9f
Jesus-GarRey <jgarrey2508@g.educaand.es>
2025-01-22 20:31:53 +0100

    cambios 2
    paco y yo

---
+++ index.js
@@ -0,0 +1,14 @@
+let paco
+paco=prompt("Escribe tu nombre");
+if (paco===("paco")) {
+    document.write("Tu nombre no es paco")
+}else{
+    document.write("Tu nombre es el correcto")
+};
+let jesus
+jesus=prompt("Escribe tu nombre");
+if (jesus===("jesus")) {
+    document.write("Tu nombre no es jesus")
+}else{
+    document.write("Tu nombre es el correcto")
+};
