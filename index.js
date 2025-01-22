e24dca4125f58d9993e733cc146ac7fc21bc3dc3
Ersheto <pcasflo2703@g.educaand.es>
2025-01-22 20:23:51 +0100

    Cambios 3
    Cambios con paco jesus y yo

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let pablo
+pablo=prompt("Escribe tu nombre");
+if(pablo===("")) {
+    document.write("Tu nombre no es pablo")
+}else{
+    document.write("Tu nombre es el correcto")
+};
