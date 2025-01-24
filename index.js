e5f96b92bb903f88377916d02f5a4309fa05ecc8
Jesus-GarRey <jgarrey2508@g.educaand.es>
2025-01-24 15:59:41 +0100

    cambio _linea

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let paco
 paco=prompt("Escribe tu nombre");
 if (paco===("paco")) {
-    document.write("Tu nombre no es paco")
-}else{
     document.write("Tu nombre es el correcto")
+}else{
+    document.write("Tu nombre no es paco")
 };
 let jesus
 jesus=prompt("Escribe tu nombre");
 if (jesus===("jesus")) {
-    document.write("Tu nombre no es jesus")
-}else{
     document.write("Tu nombre es el correcto")
+}else{
+    document.write("Tu nombre no es jesus")
 };
