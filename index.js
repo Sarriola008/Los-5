c73c06ce0d39d4dbf530807244e6377b7d8259f6
Ersheto <pcasflo2703@g.educaand.es>
2025-01-24 16:14:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 let pablo
 pablo=prompt("Escribe tu nombre");
 if(pablo===("")) {
-    document.write("Tu nombre no es pablo")
-}else{
     document.write("Tu nombre es el correcto")
+}else{
+    document.write("Tu nombre no es pablo")
 };
