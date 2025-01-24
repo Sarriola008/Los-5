11caa727270425b9709a86e5ccfc2fd83af00c8f
fran2801 <falcrod282@g.educaand.es>
2025-01-24 15:59:30 +0100

    Cambios 2

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let paco
 paco=prompt(`Escribe tu nombre()`);
 if (paco===("paco")) {
-    document.write("Tu nombre no es Paco")
-}else{
     document.write("Tu nombre es el correcto")
+}else{
+    document.write("Tu nombre no es Paco")
 };
-
