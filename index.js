74dab4c68d1d603deb0e0d1e4ad362c3da6faadb
Sarriola008 <sarrmig093@g.educaand.es>
2025-01-24 16:02:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 let Paco
 Paco=prompt("Escribe tu nombre");
 if(Paco==("")){
-    document.write("Tu nombre no es Paco")
+    document.write("Tu nombre es el correcto")
 
 }else {
-    document.write("Tu nombre es el correcto")
+    document.write("Tu nombre no es Paco")
 };
 let Jesus
 Jesus=prompt("Escribe tu nombre");
 if(Jesuss==("")){
-    document.write("Tu nombre no es Jesus")
+    document.write("Tu nombre es el correcto")
 
 }else {
-    document.write("Tu nombre es el correcto")
+    document.write("Tu nombre no es Jesus")
 };
 let Pablo
 Pablo=prompt("Escribe tu nombre");
 if(Pablo==("")){
-    document.write("Tu nombre no es Pablo")
+    document.write("Tu nombre es el correcto")
 
 }else {
-    document.write("Tu nombre es el correcto")
+    document.write("Tu nombre no es Pablo")
 };
 let Santiago
 Santiago=prompt("Escribe tu nombre");
 if(Santiago==("")){
-    document.write("Tu nombre no es Santiago")
+    document.write("Tu nombre es el correcto")
 
 }else {
-    document.write("Tu nombre es el correcto")
+    document.write("Tu nombre no es Santiago")
 };
 
 
